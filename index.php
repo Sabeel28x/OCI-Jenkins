c8be75b408b82b2cd482a89ee4fc3ed75ab9ff41
Sabeel28x <138629101+Sabeel28x@users.noreply.github.com>
2025-01-27 16:45:41 +0530

    White

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
             font-family: Arial, sans-serif;
             text-align: center;
             margin-top: 50px;
-            background-color: #99f4ae;
+            background-color: #ffffff;
         }
         h1 {
             color: #333;
